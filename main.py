e912ddff6c23a5c77aaa07ae524908ab3749f0e0
charanyachinnasamy <charanyachinnasamy@gmail.com>
2019-04-23 18:15:50 -0700

    optimizing

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 import subprocess
-subprocess.call("Test02.py", shell=True)
+subprocess.call("./Test02.py")
+subprocess.call("./CJ-web-login.py")
+subprocess.call("./CJ-web-reg-Chrome.py")
